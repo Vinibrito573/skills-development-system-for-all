29915f7bb3ad74315b58179310ee52bd2001f6d7
Vinibrito573 <viniciusmbrito@icloud.com>
2026-04-27 22:40:37 +0100

    First commit, creating user training service with Unary RPC

---
+++ client/user_training_client.js
@@ -0,0 +1,49 @@
+//USER TRAINING CLIENT - Unary RPC
+//This client connects to the User Training Service and sends a request to assign training to a user,
+//and then receive and display the response.
+
+//Importing gRPC library
+const grpc = require("@grpc/grpc-js");
+
+//Importing proto loader to be able to read the proto files
+const protoLoader =require("@grpc/proto-loader");
+
+//Importing the path to locate the files
+const path = require("path");
+
+const PROTO_PATH = path.join(__dirname, "../protos/user_training.proto");
+
+//Proto file definition
+const packageDefinition =protoLoader.loadSync(PROTO_PATH);
+
+//Creating an object converting proto
+const userTrainingProto=grpc.loadPackageDefinition(packageDefinition).usertraining;
+
+//Setting Client
+
+//Creating a client that will be able to connect to User Training Service which will be ruinning in port 50051
+const client = new userTrainingProto.UserTrainingService("localhost:50051", //server port and address
+  grpc.credentials.createInsecure());
+
+//Below we will be requesting the data
+const request = {
+    userId: 1,
+    dateOfBirth: "2000-07-07",
+    employmentStatus: "unemployed",
+    trainingTitle: "Basic Excel Skills",
+    category: "Basic Skills",
+    dueDate: "2026-05-07"
+};
+
+//Connecting RPC
+//AssignTraining Method
+client.AssignTraining(request,(error, response) =>{
+    // If there is an error
+    if (error) {
+    console.error("Error:",error);
+} 
+  else { //for action completed correctly, then display the message
+    console.log("Response from server:");
+    console.log(response);
+  }
+});
---
+++ services/user_training_server.js
@@ -0,0 +1,78 @@
+//USER TRAINING SERVICE - gRPC Unary RPC
+//The service below will be responsible for assigning training to users and validating the input data.
+
+//Starting importing gRPC library
+const grpc = require ("@grpc/grpc-js");
+
+//Importing proto loader to read proto files
+const protoLoader = require ("@grpc/proto-loader");
+
+//Importing path to be able to locate files
+const path=require ("path");
+const PROTO_PATH=path.join(__dirname, "../protos/user_training.proto");
+
+//Loading the proto file definition
+const packageDefinition =protoLoader.loadSync(PROTO_PATH);
+
+//Creating an object so we need to convert the proto
+const userTrainingProto=grpc.loadPackageDefinition (packageDefinition).usertraining;
+
+
+//Implementing the Unary RPC:
+
+//Creating a function that will be responsible for handling AssignTraining Requests
+function assignTraining(call, callback){
+    
+    //getting data from the user
+    const request=call.request;
+    
+
+//Validation and error handling
+if (!request.userId || request.userId <=0){
+    callback(null,{
+        enrollmentId: "",
+        success:false,
+        message: "Sorry, The User ID is invalid! Please enter a positive number."
+        });
+        return;
+}
+
+//Checking if the training title is valid
+if (!request.trainingTitle){
+    callback(null,{
+        enrollmentId: "",
+        success: false,
+        message: "Please enter the Training Title!"
+    });
+    return;
+}
+
+//Generating the enrolment ID
+const enrollmentId= "ENR-" + request.userId + "-" + Date.now();
+//Sending the confirmation
+callback (null,{
+    enrollmentId: enrollmentId,
+    success: true,
+    message: "Training has been assigned to your User ID, thank you!"
+});
+}
+
+// Setting the Server//
+
+//Creating a new gRPC server
+const server = new grpc.Server();
+
+//Linking the proto service with the function
+server.addService(userTrainingProto.UserTrainingService.service, {
+    AssignTraining: assignTraining });
+
+//Initiating the server on port 50051, as it is the default conventiion used in the gRPC examples, so each services will run\
+//in a different port to avoid issues.
+server.bindAsync( "0.0.0.0:50051", 
+    grpc.ServerCredentials.createInsecure(),
+    function (){
+        console.log ("This User Training Service is running on port 50051");
+        server.start();
+    }
+);
+
